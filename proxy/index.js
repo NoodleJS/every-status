c495bfabf15cf96cd69697102216e49a50f13c15
elrrrrrrr <elrrrrrrr@gmail.com>
2014-08-15 14:05:58 +0800

    fix json

--- proxy/index.js
+++ proxy/index.js
@@ -1,10 +1,21 @@
 var mongoose = require('mongoose');
-var setting = require('../settings.json').mongodb || {}
+
+var setting = {} ;
+
+try{
+  setting = require('../settings.json');
+} catch(err){
+  
+  if(err.code === 'MODULE_NOT_FOUND'){
+    setting = require('../settings.example.json');
+    }
+}
+
 
 var uri = 'mongodb://USER:PASS@HOST:port/DB'.replace('USER',setting.username)
                                             .replace('PASS',setting.passwd)
                                             .replace('HOST',setting.host)
-                                            .replace('DB',setting.dbname)
+                                            .replace('DB',setting.dbname);
 
 var db = mongoose.connect(uri,function(err){
   if (err) console.log(err)
