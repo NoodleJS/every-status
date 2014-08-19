ad6bab1e131924a6076bee1d26271cf07f0f8706
elrrrrrrr <elrrrrrrr@gmail.com>
2014-08-19 18:04:52 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -1,24 +1,8 @@
 var assert = require("assert")
+var db = require('../proxy/index')
+ 
+describe('Database', function(){
 
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
-
-describe('User', function(){
-  describe('#save()', function(){
-    it('should save without error', function(){
-        assert.equal(1,1)
-      });
-    })
-})
-
-describe('checkModel', function(){
   describe('init',function(){
     it('should has created & update prop', function(){
       var Piece = require('../model/piece')
@@ -28,7 +12,29 @@ describe('checkModel', function(){
 
       assert.equal(true, 'created' in _p && 'updated' in _p)
       assert.equal(true, 'created' in _u && 'updated' in _u)
-
     })
   })
+
+  describe('increment', function(done) {
+    it('should has an increment id', function(done) {
+      var Piece = require('../model/piece');
+      var _t1;
+      var _t2;
+      
+        new Piece({}).save(function(err,piece) {
+
+          _t1 = piece
+
+          new Piece({}).save(function(err,piece) {
+
+            _t2 = piece
+            assert.equal(true,_t1.id == _t2.id - 1)
+            done()
+
+          })
+        })
+      
+    })
+  })
+
 })
