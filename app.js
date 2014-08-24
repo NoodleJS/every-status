b0a619ff4426f2af1f7d4ee20aab0227cc346ed5
elrrrrrrr <elrrrrrrr@gmail.com>
2014-08-24 21:44:22 +0800

    add temp to scafford route & dev

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var db = require('./proxy/index');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var temp = require('./routes/temp');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/temp', temp);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -68,31 +68,6 @@ router.get('/god', function(req, res) {
 
 })
 
-router.get('/test', function(req, res) {
-
-    var fakeuser = {
-        avatar: 'http://tp1.sinaimg.cn/2017654444/50/5635300102/1', 
-        name: 'elrrrrrrr',
-        id: 1,
-        douban_access_token:'12345'
-    }
-    var fakepiece = {
-        id: 1,
-        content: 'Lets start from here',
-        author_name: 'elrrrrrrr', 
-        work_title: 'Just for fun',
-        addtime: '2014-08-20'
-    }
-    
-    res.render('user', { title: '今天...', 
-        name: 'people',
-        user: fakeuser,
-        current: fakeuser, 
-        pages: 1,
-        index: 1,
-        favs:[fakepiece,fakepiece]
-    })  
-})
 
 router.get('/piece/:id', con.show);
 
