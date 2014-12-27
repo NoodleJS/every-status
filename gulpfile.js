01563ba14952d47454ca91a9e7c3cdc7af105bc7
qinnnnnnnn <qinyuweifff@hotmail.com>
2014-12-27 15:44:32 +0800

    update glupfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
-    coffee = require('gulp-coffee'),
     jade = require('gulp-jade')
 
 var paths = {
   stylus: ['public/stylus/**/*.styl'],
-  coffee: ['*.coffee','./*/*.coffee'],
   jade: ['views/new/*.jade']
 };
 
@@ -15,12 +13,6 @@ gulp.task('stylus', function() {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('coffee', function() {
-  return gulp.src(paths.coffee)
-    .pipe(coffee())
-    .pipe(gulp.dest(''))
-})
-
 gulp.task('jade', function() {
   return gulp.src(paths.jade)
     .pipe(jade())
@@ -28,9 +20,8 @@ gulp.task('jade', function() {
 })
 
 gulp.task('work', function() {
-  gulp.watch(paths.coffee, ['coffee'])
   gulp.watch(paths.stylus, ['stylus'])
   gulp.watch(paths.jade, ['jade'])
 })
 
-gulp.task('default', ['stylus','coffee']);
+gulp.task('default', ['stylus', 'jade']);
