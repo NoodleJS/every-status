ef8538dc66709fd297d4222863640753aecb86e4
qinnnnnnnn <qinyuweifff@hotmail.com>
2015-01-23 23:58:58 +0800

    update author

--- new/app.js
+++ new/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var entry = require('./lib/doctor');
-
-var app = entry.createApp({
+var app=entry.createApp({
   base: __dirname
 });
 
 if (!module.parent) {
   app.listen(3000);
 }
-
